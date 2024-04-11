a0d4f8beb631cc7a6d7e8bb64e1f0be1ebcc4524
Wesley Costa <138899846+wesleycosta13@users.noreply.github.com>
2024-04-11 12:51:02 -0300

    Add files via upload

---
+++ aula11/js/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite um numero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O Resultado da sua conta foi: ${num1 + num2}`);
